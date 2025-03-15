003226cb7eb522cdb96fb0eb5ee5a90f4f3e0232
webcommando <webcommando@users.noreply.github.com>
2025-03-15 12:32:20 -0500

    Note App Added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       "@aws-amplify/ui-react/styles.css", // ignore react stuff
+  //       "@aws-amplify/ui-react", // ignore react stuff
+        
+  //     ],
+  //   },
+  // },
 })
